e98e8c8e944b518206063f3179659ba548eb6f5a
quintenvanvelzen <quinten@dexterenergy.ai>
2024-07-14 19:55:47 +0200

    :sparkles: Creating Bank class and first file structure

--- src/bank.py
+++ src/bank.py
@@ -9,8 +9,8 @@ class Bank:
     @staticmethod
     def load_data():
         from fake_data_base.data import hardcoded_data
-        hardcoded_bank_data = pd.DataFrame(hardcoded_data)
-        return hardcoded_data
+        hardcoded_df = pd.DataFrame(hardcoded_data)
+        return hardcoded_df
 
     def check_balance(self):
         df = self.load_data()
